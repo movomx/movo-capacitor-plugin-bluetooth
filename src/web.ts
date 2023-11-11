0cc8cc3e7c904b0cdb486d7a31bd07f7efedf7d0
alex <alex.guajardo@inroute.com.mx>
2023-11-10 17:10:17 -0700

    Creating classes

--- src/web.ts
+++ src/web.ts
@@ -3,6 +3,16 @@ import { WebPlugin } from '@capacitor/core';
 import type { BluetoothPlugin } from './definitions';
 
 export class BluetoothWeb extends WebPlugin implements BluetoothPlugin {
+  async scan(options: { filter: Array<string> }): Promise<{ devices: Array<{ bluetooth_name: string, mac_address: string, state: string }> }> {
+    console.log('scan', options);
+    return options;
+  }
+
+  async onDeviceFound(): Promise<{ bluetooth_name: string, mac_address: string, state: string }> {
+    console.log('onDeviceFound');
+    return null;
+  }
+
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
